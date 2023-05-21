b62b8808e0031d52dae003fb56d2c58a423fc1e8
Jynx <thekillercraftlp@gmail.com>
2023-05-21 23:58:30 +0200

    fetching elements via api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,66 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+</script>
+
 <svelte:head>
-    <title>Genshin Impact Calculator</title>
+    <title>Genshinfo</title>
 </svelte:head>
 
-<h1>Welcome to the Genshin Impact Calculator</h1>
+<main>
+    <h1>Welcome to Genshinfo</h1>
+    <section id="elements">
+        <h2>Elements</h2>
+        <div>
+            {#each data.elements as element}
+            <a href="/elements/{element}" class="element">
+                <img src="https://api.genshin.dev/elements/{element}/icon" alt="{element} icon">
+                <span>{element.charAt(0).toUpperCase() + element.substring(1)}</span>
+            </a>
+            {/each}
+        </div>
+    </section>
+</main>
 
 <style>
-    h1 {
-        margin: auto;
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    main > section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        border: 1px solid gray;
+        width: 80vw;
+    }
+
+    #elements > div {
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
+    }
+
+    #elements > div > a {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .element {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100px;
+        height: 100px;
+    }
+
+    .element:hover {
+        background-color: rgba(255,255,255,0.1);
+        border-radius: 10px;
     }
 </style>
